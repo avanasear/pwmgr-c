2cde9b261f82ea55f5fb417f2a51a230bb613803
Zach Kaylor <avanasear@gmail.com>
2019-09-29 00:13:53 -0400

    expanded upon initialize()

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <gcrypt.h>
-#include <json/json.h>
+//#include <json/json.h>
 
 int is_initialized(const char * dir, const char * shadow, const char * passes){
 	/* Function to tell if:
@@ -34,30 +34,55 @@ void initialize(const char * dir, const char * shadow, const char * passes){
 	// Create the necessary files and directory.
 
 	struct stat st = { 0 };
+	int initialized = 0;
+	char * passwd;
+	char * repeat;
 
 	if (stat(dir, &st) == -1){
 		mkdir(dir, 0700);
 	}
 	else {
 		printf("~/.pwmgr already exists\n");
+		initialized++;
 	}
 
 	if (stat(shadow, &st) == -1){
 		FILE * shadowptr = fopen(shadow, "w");
 		fputs("\n", shadowptr);
+		fclose(shadowptr);
 		chmod(shadow, 0600);
 	}
 	else {
 		printf("~/.pwmgr/shadow.pw already exists\n");
+		initialized++;
 	}
 
 	if (stat(passes, &st) == -1){
 		FILE * passesptr = fopen(passes, "w");
 		fputs("[]", passesptr);
+		fclose(passesptr);
 		chmod(passes, 0600);
 	}
 	else {
 		printf("~/.pwmgr/passes.pw already exists\n");
+		initialized++;
+	}
+	
+	if (initialized < 1){
+		passwd = getpass("Create a password: ");
+		repeat = getpass("Confirm your password: ");
+		if (strcmp(passwd, repeat) == 0){
+			// Passwords MATCH
+			FILE * shadowptr = fopen(shadow, "w");
+			// hash the password
+			// fputs(hash, shadow);
+			fclose(shadowptr);
+			
+		}
+		else {
+			// Passwords DO NOT MATCH
+			exit(1);
+		}
 	}
 }
 
@@ -78,10 +103,10 @@ int main(int argc, const char * argv[]){
 	strncpy(passes, dir, strlen(dir));
 	strncat(passes, "/passes.pw", strlen("/passes.pw"));
 
-	printf("%d\n", is_initialized(dir, shadow, passes));
+	//printf("%d\n", is_initialized(dir, shadow, passes));
 	initialize(dir, shadow, passes);
-	printf("%s\n%s\n", home, dir);
-	printf("%d\n", is_initialized(dir, shadow, passes));
+	//printf("%s\n%s\n", home, dir);
+	//printf("%d\n", is_initialized(dir, shadow, passes));
 
 	return 0;
 }
