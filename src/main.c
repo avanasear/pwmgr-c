2699f000d33dabda48e02bbaf718acaf24cdfea0
Zach Kaylor <avanasear@gmail.com>
2019-09-30 21:31:18 -0400

    expanding upon initializiation

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <gcrypt.h>
 //#include <json/json.h>
+#include <termios.h>
 
 int is_initialized(const char * dir, const char * shadow, const char * passes){
 	/* Function to tell if:
@@ -30,13 +31,34 @@ int is_initialized(const char * dir, const char * shadow, const char * passes){
 	}
 }
 
+struct termios saved_attributes;
+
+void reset_input_mode(){
+	tcsetattr(STDIN_FILENO, TCSANOW, &saved_attributes);
+}
+
+void set_input_mode(){
+	struct termios tattr;
+	if (!isatty (STDIN_FILENO)){
+		fprintf(stderr, "Not a terminal.\n");
+		exit(1);
+	}
+	tcgetattr(STDIN_FILENO, &saved_attributes);
+	atexit(reset_input_mode);
+	tcgetattr(STDIN_FILENO, &tattr);
+	tattr.c_lflag &= ~(ICANON | ECHO);
+	tattr.c_cc[VMIN] = 1;
+	tattr.c_cc[VTIME] = 0;
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &tattr);
+}
+
 void initialize(const char * dir, const char * shadow, const char * passes){
 	// Create the necessary files and directory.
 
 	struct stat st = { 0 };
 	int initialized = 0;
-	char * passwd;
-	char * repeat;
+	char passwd[100] = {0};
+	char repeat[100] = {0};
 
 	if (stat(dir, &st) == -1){
 		mkdir(dir, 0700);
@@ -69,16 +91,24 @@ void initialize(const char * dir, const char * shadow, const char * passes){
 	}
 	
 	if (initialized < 1){
-		// REPLACE. THIS. WITH. SOMETHING. ELSE. USE TERMIOS OR SOMETHING.
-		passwd = getpass("Create a password: ");
-		repeat = getpass("Confirm your password: ");
+		// get passwd and repeat
+		set_input_mode();
+
+		printf("Create a password: ");
+		fgets(passwd, 100, stdin);
+		//printf("\npasswd = %s\n", passwd);
+		printf("Repeat your password: ");
+		fgets(repeat, 100, stdin);
+		//printf("\nrepeat = %s\n", repeat);
+
+		reset_input_mode();
 		if (strcmp(passwd, repeat) == 0){
 			// Passwords MATCH
 			FILE * shadowptr = fopen(shadow, "w");
 			// hash the password
+			//gcry_md_open(GCRY_MD_SHA512, 1, )
 			// fputs(hash, shadow);
 			fclose(shadowptr);
-			
 		}
 		else {
 			// Passwords DO NOT MATCH
